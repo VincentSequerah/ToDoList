cd148f985697326f517635911b9c3f17800f859d
VincentSequerah <v1jnicholas@gmail.com>
2024-09-09 01:03:30 +0700

    update

--- main.js
+++ main.js
@@ -3,23 +3,23 @@ window.addEventListener('load', () => {
 	const input = document.querySelector("#new-task-input");
 	const list_el = document.querySelector("#tasks");
 
-	form.addEventListener('submit', (e) => {
-		e.preventDefault();
+	form.addEventListener('submit', (event) => {
+		event.preventDefault();   //cancels submit from submitting form//
 
 		const task = input.value;
 
         if (!task){
             alert ('No text entered');
             return;
-        }
+        }   //check to see if any text entered when submitted//
 
 		const task_el = document.createElement('div');
-		task_el.classList.add('task');
+		task_el.classList.add('task');    //create div for new task//
 
 		const task_content_el = document.createElement('div');
-		task_content_el.classList.add('content');
+		task_content_el.classList.add('content');   //create div for user input//
 
-		task_el.appendChild(task_content_el);
+		task_el.appendChild(task_content_el); //append user input to new task//
 
 		const task_input_el = document.createElement('input');
 		task_input_el.classList.add('text');
